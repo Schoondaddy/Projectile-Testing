37d04f3f826c5c6577524bc08f9c95cadc155cde
Schoondaddy <Xavierbunbunny@gmail.com>
2024-06-05 22:07:21 -0700

    IT WORKS IM SUPA PRO

--- Bouncing projectile test/NavigationGenerator.gd
+++ Bouncing projectile test/NavigationGenerator.gd
@@ -17,8 +17,6 @@ func _ready():
 	bake_navigation_polygon()
 
 func add_boundary():
-	
-	
 	var outline = PackedVector2Array([
 		Vector2(leftmost_bound,upper_bound),
 		Vector2(rightmost_bound,upper_bound),
@@ -26,38 +24,22 @@ func add_boundary():
 		Vector2(leftmost_bound,lower_bound)
 	])
 	nav_poly.add_outline(outline)
-	
-	
-	
+
 
 
 func generate_polygon():
-	var list_of_polygons = []
+	var list_of_polygons = [] #This will be where all of the polygons will be stored
 	var tile_set = tileMap.tile_set
-	
 	for cell in tileMap.get_used_cells(0):
 		var tile_id = tileMap.get_cell_source_id(0, cell)
-		if tile_id != -1:
+		if tile_id != -1: #Check if the tile exists
 			var tile_size = tile_set.tile_size
-			var polygon = tileMap.get_cell_tile_data(0, cell).get_collision_polygon_points(0,0)
-			print (polygon)
-			for i in polygon.size():
-				polygon[i].x = polygon[i].x + (cell.x * tile_size.x)
-				polygon[i].y = polygon[i].y + (cell.y * tile_size.y)
-			
-			
-			list_of_polygons.append(polygon)
+			var polygon = tileMap.get_cell_tile_data(0, cell).get_collision_polygon_points(0,0) #Getting the shape of the tilemap tile
+			for i in polygon.size(): #Correcting size and position of polygon points
+				polygon[i].x = 0.0625 * polygon[i].x + 8 + cell.x * tile_size.x
+				polygon[i].y = 0.0625 * polygon[i].y + 8 + cell.y * tile_size.y
+			list_of_polygons.append(polygon) #Adding finished polygons to list of polygons
 	
-	var merged_polygon = PackedVector2Array()
 	for polygon in list_of_polygons:
-		merged_polygon = Geometry2D.merge_polygons(merged_polygon, polygon)
-	merged_polygon = merged_polygon[0]
-	print(merged_polygon)
-	merged_polygon.reverse()
-	print(merged_polygon)
-	
-	
-	print(merged_polygon)
-	var new_vertices = PackedVector2Array([Vector2(0, 0), Vector2(0, 50), Vector2(50, 50), Vector2(50, 0)])
-	nav_poly.add_outline(merged_polygon)
-
+		polygon.reverse() #Puts the points in correct clockwise order
+		nav_poly.add_outline(polygon) #Adds the outline of the polygon to the navigation polygon
